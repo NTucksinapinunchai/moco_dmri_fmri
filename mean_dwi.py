3ccae9d52fa571fc3a0e3658ed055e0452b87560
NTucksinapinunchai <nontharatt@gmail.com>
2025-09-23 16:14:01 -0400

    Update script

--- mean_dwi.py
+++ mean_dwi.py
@@ -101,15 +101,41 @@ for subfolder in subfolders:
     except subprocess.CalledProcessError as e:
         print("Error", e)
 
+    nii_files = glob.glob(os.path.join(subfolder, "dwi", "sub-*b0_mean.nii.gz"))
+    if len(nii_files) == 0:
+        raise FileNotFoundError("No NIfTI file found in folder.")
+    mean_b0_img = nii_files[0]
+    print("Found NIfTI:", mean_b0_img)
+
     # ---------------------------
     # Duplicate dwi_mean along time point
     # ---------------------------
-    mean_img = nib.load(mean_img)
-    mean_data = mean_img.get_fdata()
-    mean_4d = np.repeat(mean_data[..., np.newaxis], T, axis=3)
-    dup_nii = nib.Nifti1Image(mean_4d, affine=mean_img.affine, header=mean_img.header)
+    nii = nib.load(input_img)
+    data = nii.get_fdata()
+    affine = nii.affine
+    header = nii.header
 
-    basename_noext = os.path.basename(input_img).replace(".nii.gz", "")
-    out_4d_mean = os.path.join(subfolder, "dwi", f"dup_{basename_noext}_mean.nii.gz")
-    nib.save(dup_nii, out_4d_mean)
-    print("Saved:", out_4d_mean)
+    # Load mean_b0
+    mean_b0_nii = nib.load(mean_b0_img)
+    mean_b0_data = mean_b0_nii.get_fdata()
+
+    # Load mean_dwi
+    mean_dwi_nii = nib.load(mean_img)
+    mean_dwi_data = mean_dwi_nii.get_fdata()
+
+    # Load bval to know which volumes are b0 and which are dwi
+    bvals = np.loadtxt(bval_file)
+
+    # Replace volumes according to bval
+    corrected = np.zeros_like(data)
+    for t in range(T):
+        if bvals[t] < 50:  # identify b0
+            corrected[..., t] = mean_b0_data
+        else:  # identify dwi
+            corrected[..., t] = mean_dwi_data
+
+    # Save single corrected 4D file
+    basename_noext = os.path.basename(input_img).replace("_dwi.nii.gz", "")
+    out_corrected = os.path.join(subfolder, "dwi", f"dup_{basename_noext}_fixed.nii.gz")
+    nib.save(nib.Nifti1Image(corrected, affine, header), out_corrected)
+    print("Saved:", out_corrected)
